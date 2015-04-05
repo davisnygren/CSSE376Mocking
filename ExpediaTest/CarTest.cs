84be07e549830a227bac23b899a341a3afd130ed
davisnygren <davisnygren@gmail.com>
2015-04-05 17:53:33 -0500

    Object mother in lab

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -50,5 +50,55 @@ namespace ExpediaTest
 		{
 			new Car(-5);
 		}
+
+        [TestMethod]
+        public void TestThatCarLocationWorks()
+        {
+            targetCar = new Car(24);
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            targetCar.Database = mockDB;
+            String loc = "Panama City Beach";
+            String loc2 = "Neverland";
+
+            Expect.Call(mockDB.getCarLocation(24)).Return(loc);
+            Expect.Call(mockDB.getCarLocation(1024)).Return(loc2);
+
+            mocks.ReplayAll();
+
+            String result = targetCar.getCarLocation(24);
+            Assert.AreEqual(loc, result);
+
+            targetCar = new Car(1024);
+            targetCar.Database = mockDB;
+            result = targetCar.getCarLocation(1024);
+            Assert.AreEqual(loc2, result);
+        }
+
+        [TestMethod]
+        public void TestThatCarMileageWorks()
+        {
+            IDatabase mockDatabase = mocks.StrictMock<IDatabase>();
+            int mileage = 1295;
+            int mileage2 = 12358;
+
+            targetCar = new Car(10);
+            targetCar.Database = mockDatabase;
+
+            Expect.Call(targetCar.Mileage).Return(mileage);
+
+            mocks.ReplayAll();
+
+            Assert.AreEqual(targetCar.Mileage, mileage);
+
+            targetCar = new Car(1024);
+            targetCar.Database = mockDatabase;
+
+            mockDatabase.Expect(x => x.Miles).Return(mileage2);
+
+            Assert.AreEqual(mileage2, targetCar.Mileage);
+
+            mocks.VerifyAll();
+
+        }
 	}
 }
--- ExpediaTest/ObjectMother.cs
+++ ExpediaTest/ObjectMother.cs
@@ -1,5 +1,7 @@
 using Expedia;
 using System;
+using Expedia.Car;
+
 
 namespace ExpediaTest
 {
@@ -7,4 +9,14 @@ namespace ExpediaTest
 	{
 		
 	}
+
+    public static Car Saab()
+    {
+        return new Car(7) { Name = "Saab 9-5 Sports Sedan" };
+    }
+
+    public static Car BMW()
+    {
+        return new Car(12) { Name = "1979 BMW 323i German Import" };
+    }
 }
